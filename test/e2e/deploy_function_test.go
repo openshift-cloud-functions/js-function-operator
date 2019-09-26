e1a852cd92e8ae6519580577fae085f43021c9fe
Lance Ball <lball@redhat.com>
2019-09-26 18:08:59 -0400

    build,test: tests for recent changes and add CI (#27)
    * test: update e2e test to work with recent changes
    
    * applied some go formatting via VSCode extension.
    
    * build: add circle ci config

--- test/e2e/deploy_function_test.go
+++ test/e2e/deploy_function_test.go
@@ -2,13 +2,14 @@ package e2e
 
 import (
 	goctx "context"
+	"testing"
+
 	jsfunction "github.com/openshift-cloud-functions/js-function-operator/pkg/apis/faas/v1alpha1"
 	"github.com/openshift-cloud-functions/js-function-operator/test"
 	framework "github.com/operator-framework/operator-sdk/pkg/test"
 	"github.com/stretchr/testify/assert"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	knservingclient "knative.dev/serving/pkg/client/clientset/versioned"
-	"testing"
 )
 
 var (
@@ -21,6 +22,7 @@ var (
 
 func TestDeployFunction(t *testing.T) {
 	ctx, f, namespace := test.E2EBootstrap(t)
+	defer ctx.Cleanup()
 
 	t.Logf("Deploying function in namespace %v", namespace)
 
@@ -42,7 +44,7 @@ func TestDeployFunction(t *testing.T) {
 
 	// Wait for knative service to be ready
 	knService := test.WaitForKnativeServiceReadyDefault(t, &knClient, functionName, namespace)
-	serviceURL := knService.Status.Address.URL
+	serviceURL := knService.Status.URL
 
 	test.AssertGetRequest(t, serviceURL.String(), 200, []byte("ok"))
 
